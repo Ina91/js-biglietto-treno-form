9f71ad337979f1b327fa9cfe9ba295dd34fb9815
Ina91 <ina.met91@gmail.com>
2020-07-16 17:48:53 +0200

    modifica fz calcola al click di genera

--- js/script.js
+++ js/script.js
@@ -1,24 +1,46 @@
+// sconto
+document.getElementById("genera").addEventListener("click",offertaFunction );
+function offertaFunction() {
+    var eta = document.getElementById('age-input').value;
+    if (eta < 18){
+        document.getElementById('offerta').innerHTML= 'Sconto Minorenne';
+    }else if (eta>65) {
+        document.getElementById('offerta').innerHTML= 'Sconto Silver';
+
+    }else{
+        document.getElementById('offerta').innerHTML= 'Tariffa Standart';
+
+    }
+
+}
+
 // numeri  casuali carrozza da 1  a 10
- var carrozza = Math.floor(Math.random()*10)+1;
- document.getElementById('carrozza').innerHTML = carrozza;
+document.getElementById("genera").addEventListener("click",carrozzaFunction );
+function carrozzaFunction(carro){
+    var carrozza = Math.floor(Math.random()*10)+1;
+    document.getElementById('carrozza').innerHTML = carrozza;
+}
 
  // numero codice-cp da 90000 e 100000 escluso
- var codiceCp = Math.floor(Math.random()*99999)+90000 ;
- document.getElementById('codice-cp').innerHTML = codiceCp;
+ document.getElementById("genera").addEventListener("click", codiceCpFunction);
+ function codiceCpFunction(codice){
+     var codiceCp = Math.floor(Math.random()*99999)+90000 ;
+     document.getElementById('codice-cp').innerHTML = codiceCp;
+ }
 
  // costo biglietto
  document.getElementById("genera").addEventListener("click", miaFunzione);
  function miaFunzione(distanza,eta,costo) {
-    var distanza = document.getElementById('distance-input').value;
-    var eta = document.getElementById('age-input').value;
-    var costoKm = 0.21;
-    var costo = distanza * costoKm;
+     var distanza = document.getElementById('distance-input').value;
+     var eta = document.getElementById('age-input').value;
+     var costoKm = 0.21;
+     var costo = distanza * costoKm;
 
-    if (eta<18) {
-        costo *= 0.8;
-    }else if (eta>65) {
-        costo *= 0.6;
-    }
-    document.getElementById('costo').innerHTML = costo.toPrecision(3) + '€';
+     if (eta<18) {
+         costo *= 0.8;
+     }else if (eta>65) {
+         costo *= 0.6;
+     }
+     document.getElementById('costo').innerHTML = costo.toPrecision(3) + '€';
 
-}
+ }
