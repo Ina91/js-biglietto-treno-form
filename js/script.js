88bc8e57c6cfbcd616f8ab4f66ee869b15ea1d0f
Ina91 <ina.met91@gmail.com>
2020-07-16 17:23:10 +0200

    inserimendo fz random per carrozza e codice-cp

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// numeri  casuali carrozza da 1  a 10
+ var carrozza = Math.floor(Math.random()*10)+1;
+ document.getElementById('carrozza').innerHTML = carrozza;
+
+ // numero codice-cp da 90000 e 100000 escluso
+ var codiceCp = Math.floor(Math.random()*99999)+90000 ;
+ document.getElementById('codice-cp').innerHTML = codiceCp;
+
+ // costo biglietto
+ function miaFunzione(distanza,eta,costo) {
+    var distanza = document.getElementById('distance-input').value;
+    var eta = document.getElementById('age-input').value;
+    var costoKm = 0.21;
+    var costo = distanza * costoKm;
+
+    if (eta<18) {
+        costo *= 0.8;
+    }else if (eta>65) {
+        costo *= 0.6;
+    }
+    document.getElementById('costo').innerHTML =  + costo.toPrecision(3) + '€';
+
+}
